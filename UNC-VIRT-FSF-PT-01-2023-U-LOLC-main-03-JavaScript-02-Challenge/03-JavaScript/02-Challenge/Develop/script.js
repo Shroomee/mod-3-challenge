67f09a5e5606146b5f94d042417dd334b98c175f
unknown <dudeomous@gmail.com>
2023-02-12 01:32:37 -0500

    cleaned up little prompt errors

--- UNC-VIRT-FSF-PT-01-2023-U-LOLC-main-03-JavaScript-02-Challenge/03-JavaScript/02-Challenge/Develop/script.js
+++ UNC-VIRT-FSF-PT-01-2023-U-LOLC-main-03-JavaScript-02-Challenge/03-JavaScript/02-Challenge/Develop/script.js
@@ -5,7 +5,7 @@ var specialChar = "!#$%&'()*+,-./:;<=>?@][^_`{|}~"
 var numerics = "0123456789"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var passwordLength = 0
-var isLowercase= false
+var isLowercase = false
 var isUppercase = false
 var isNumerics = false
 var isSpecialChar = false
@@ -13,65 +13,81 @@ var answer = ""
 var combineChar = ""
 
 //function for verifying password lengths
-function verifyPasswordLength(){
+function verifyPasswordLength() {
   passwordLength = window.prompt("How long is your password?")
-  if(isNaN(passwordLength)){
+  if (isNaN(passwordLength)) {
     window.alert("password length must be a number between 8-128")
     verifyPasswordLength()
   }
-  if(passwordLength<8){
+  if (passwordLength < 8) {
     window.alert("Password should be more than 8 characters!")
     verifyPasswordLength()
   }
-  else if(passwordLength>128){
-  window.alert("Password has to be less than 128 characters!")
+  else if (passwordLength > 128) {
+    window.alert("Password has to be less than 128 characters!")
     verifyPasswordLength()
-  } 
+  }
 
-verifyCharTypes()
+  verifyCharTypes()
 }
 
 
 //prompts for specific password requests
-function verifyCharTypes(){
-  answer = window.prompt("Do you want lowercase characters? Yes or No?");
-    if (answer == "yes"){
-      isLowercase = true
-      console.log(isLowercase)
-    }
-   answer = window.prompt("Do you want Uppercase characters? Yes or No?")
-    if (answer == "yes"){
-      isUppercase = true
-      console.log(isUppercase)
-    }
-   answer = window.prompt("Do you want Numbers? Yes or No?")
-    if (answer == "yes"){
-      isNumerics = true
-      console.log(isNumerics)
-    }
-    answer = window.prompt("Do you want Special characters? Yes or No?")
-    if (answer == "yes"){
-      isSpecialChar = true
-      console.log(isSpecialChar)
-    }
-    if (isLowercase == false && isNumerics == false && isSpecialChar == false && isUppercase == false){
-      window.alert("You must select at least one character type")
-      verifyCharTypes()
-    }
+function verifyCharTypes() {
+  answer = window.prompt("Do you want lowercase characters? yes or no?");
+  if (!(answer == "yes" || answer == "no")) {
+    window.alert("please say yes or no")
+    verifyCharTypes()
+  }
+  if (answer.toLowerCase() === "yes") {
+    isLowercase = true
+    console.log(isLowercase)
+  }
+  answer = window.prompt("Do you want Uppercase characters? yes or no?")
+  if (!(answer == "yes" || answer == "no")) {
+    window.alert("please say yes or no")
+    verifyCharTypes()
+  }
+  if (answer.toLowerCase() === "yes") {
+    isUppercase = true
+    console.log(isUppercase)
+  }
+  answer = window.prompt("Do you want Numbers? yes or no?")
+  if (!(answer == "yes" || answer == "no")) {
+    window.alert("please say yes or no")
+    verifyCharTypes()
+  }
+  if (answer.toLowerCase() === "yes") {
+    isNumerics = true
+    console.log(isNumerics)
+  }
+  answer = window.prompt("Do you want Special characters? yes or no?")
+  if (!(answer == "yes" || answer == "no")) {
+    window.alert("please say yes or no")
+    verifyCharTypes()
+  }
+  if (answer.toLowerCase() === "yes") {
+    isSpecialChar = true
+    console.log(isSpecialChar)
+  }
+  if (isLowercase == false && isNumerics == false && isSpecialChar == false && isUppercase == false) {
+    window.alert("You must select at least one character type")
+    verifyCharTypes()
+  }
   createPasswordCharPool()
 }
 // function to make a single string to choose things from
-function createPasswordCharPool(){
-  if(isLowercase == true){
+function createPasswordCharPool() {
+  if (isLowercase == true) {
     combineChar = combineChar + lowerCase
   }
-  if(isUppercase == true){
+  if (isUppercase == true) {
     combineChar = combineChar + upperCase
   }
-  if(isNumerics == true){
+  if (isNumerics == true) {
     combineChar = combineChar + numerics
   }
-  if(isSpecialChar == true){
+  if (isSpecialChar == true) {
     combineChar = combineChar + specialChar
   }
   writePassword()
@@ -79,10 +95,10 @@ function createPasswordCharPool(){
 }
 
 //function for choosing randomly what password is
-function generatePassword(){
+function generatePassword() {
   console.log(passwordLength)
-  for (var i = 0; i<=passwordLength; i++){
-    password += combineChar.charAt(Math.floor(Math.random()*combineChar.length))
+  for (var i = 0; i <= passwordLength; i++) {
+    password += combineChar.charAt(Math.floor(Math.random() * combineChar.length))
   }
   return password
 }
@@ -95,7 +111,7 @@ verifyPasswordLength()
 
 // Write password to the #password input
 function writePassword() {
-  password=""
+  password = ""
   password = generatePassword();
   var passwordText = document.querySelector("#password");
 
